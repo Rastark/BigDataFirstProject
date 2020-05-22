3d361d2361b3ad1333e69fe87af4d509ed2b9311
Alessandro Aurora <alx.aurora@hotmail.it>
2020-05-22 11:48:18 +0200

    Spark project setup

---
+++ src/main/java/spark/App.java
@@ -0,0 +1,14 @@
+package spark;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+
+/**
+ * Launches MapJoinJob
+ *
+ */
+public class App {
+    public static void main(final String[] args) {
+
+    }
+}
